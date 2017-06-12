c58598512bec9a5e5e30800cb45fbf797a1077e2
LeoLam <leolam@LeoLams-Mac.local>
2017-06-13 01:37:30 +0700

    done

--- todo.js
+++ todo.js
@@ -1,3 +1,25 @@
-function TodoListCtroller($scope){
-    $scope.Todos = 3;
+function TodoCtroller($scope){
+    $scope.TodosCount = 3;
+
+    $scope.Todos = [{text:"Learn from leo", checked:false},
+                    {text:"leo lam", checked:false},
+                    {text:"leolao1965", checked:false},
+                    {text:"leoleogmail", checked:false}];
+
+    $scope.getTotalTodos = function() {
+        return $scope.Todos.length;
+    }
+
+    $scope.deleteTodos = function() {
+        $scope.Todos = _.filter($scope.Todos, 
+        function(todo){
+            return !todo.checked;
+        }
+        )
+    }
+
+    $scope.addTodo = function() {
+        $scope.Todos.push({text:$scope.formTodoText,checked:false});
+        $scope.formTodoText = "";
+    }
 }
